f7d6cbce66106354334d07176a15329f6bdab534
Hoang#2 <Hoang2@Hoangs-MacBook-Pro.local>
2021-04-08 15:11:41 +0700

    final version

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux'
 import {fetchStream}from '../../actions'
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+        // Access DOM element to create the stream player as teh web load up FIRST
+        // equal to get elemtById
+        this.videoRef = React.createRef()
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        const { id } = this.props.match.params;
+
+        this.props.fetchStream(id)
+        this.buildPlayer();
+        
+    }
+
+    // anytime the component is rendered, we try to build the player again
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    //clean up component to avoid data keep fecthing to the player
+    componentWillMount() {
+        this.player.destroy()
+    }
+
+    buildPlayer() {
+
+        // if there is alreay player exits or there is no data of the stream
+        if(this.player || !this.props.stream) {
+            return; // return nothing
+        }
+
+        //only set up the player when the data is fetched
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer( {
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load();
+
+
     }
 
     render() {
@@ -16,6 +57,7 @@ class StreamShow extends React.Component {
 
         return (
             <div>
+                <video ref={this.videoRef} style={{width: '100%'}} controls/>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
